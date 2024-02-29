9bb4fa484cbf13b4eed7961302019c8b6f1618cc
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-29 15:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.querySelector('input[type="button"]');
+  var select = document.getElementById("colorSelect");
+  
+  button.addEventListener("click", function() {
+    var selectedIndex = select.selectedIndex;
+    
+    if (selectedIndex !== -1) {
+      select.remove(selectedIndex);
+    }
+  });
+});
